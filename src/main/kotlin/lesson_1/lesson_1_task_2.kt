ce5613ae85ab53e105cdf5826d857d7977f6ff4f
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-07 17:12:06 +0300

    KS-1-2 Solution for task 2 of lesson 1

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,14 @@
+package lesson_1
+
+fun main(){
+    val numOfOrders: Int = 75
+    val thankMessage: String = "Спасибо за покупку и поддержку нашего интернет-магазина!"
+    println(numOfOrders)
+    println(thankMessage)
+
+    var numberOfEmployees: Int = 2000
+    // println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+}
