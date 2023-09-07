44c52df88acbda0544434869d3ee992705c92d9f
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-07 17:20:37 +0300

    KS-1-5 Solution for task 5 of lesson 1

---
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -0,0 +1,14 @@
+package lesson_1
+
+fun main(){
+    val hours: String = "01"
+    val minutes: String = "48"
+    val seconds: String = "00"
+    val colonChar: Char = ':'
+
+    print(hours)
+    print(colonChar)
+    print(minutes)
+    print(colonChar)
+    print(seconds)
+}
