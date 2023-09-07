09f7afb9a6f5176e00f784c43c9b83c8d27fc432
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-07 17:24:00 +0300

    KS-2-1 Solution for task 1 of lesson 2

---
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -0,0 +1,10 @@
+package lesson_2
+
+fun main(){
+    val pupilA = 3f
+    val pupilB = 4f
+    val pupilC = 3f
+    val pupilD = 5f
+    val averageScore = (pupilA + pupilB+ pupilC + pupilD) / 4
+    println(averageScore)
+}
