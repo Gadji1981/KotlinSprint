e18fc90b7c7cb1ba65e02dc486d878f198b912c3
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-08 16:24:13 +0300

    KS-3-4 Solution for task 4 of lesson 3 updated

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -4,13 +4,13 @@ fun main() {
     var moveFrom = "E2"
     var moveTo = "E4"
     var moveCount = 1
-    val playerOne = "$moveFrom - $moveTo : $moveCount"
+    val playerOne = "$moveFrom-$moveTo;$moveCount"
     println(playerOne)
 
     moveFrom = "D2"
     moveTo = "D3"
     moveCount = 2
-    val playerTwo = "$moveFrom - $moveTo : $moveCount"
+    val playerTwo = "$moveFrom-$moveTo;$moveCount"
     println(playerTwo)
 
 }
