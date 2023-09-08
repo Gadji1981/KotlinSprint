5b955beec3a34a30378f260927640ba393487641
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-08 16:17:53 +0300

    KS-3-5 Solution for task 5 of lesson 3 updated

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -3,15 +3,11 @@ package lesson_3
 fun main() {
     val playerMove = "D2-D4:0"
 
-    val stringSplitOne = playerMove.split(":")
+    val stringSplit = playerMove.split(":","-")
 
-    val moveData = stringSplitOne[0]
-    val moveNum = stringSplitOne[1]
-
-    val stringSplitTwo = moveData.split("-")
-
-    val moveFrom = stringSplitTwo[0]
-    val moveTo = stringSplitTwo[1]
+    val moveFrom = stringSplit[0]
+    val moveTo = stringSplit[1]
+    val moveNum = stringSplit[2]
 
     println(moveFrom)
     println(moveTo)
