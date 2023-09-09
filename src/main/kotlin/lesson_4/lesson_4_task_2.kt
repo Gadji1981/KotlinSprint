baef0805c466b9c88edbd0848aba15f3692182b1
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-09 13:37:21 +0300

    KS-4-2 Solution for task 2 of lesson 4 updated

--- src/main/kotlin/lesson_4/lesson_4_task_2.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_2.kt
@@ -4,7 +4,6 @@ fun main() {
     val averageWeightRange = 35..100
     val averageVolumeRange = 100
 
-    println("Груз с весом 42 кг и объемом 120 л соответствует категории 'Average': ${42 in averageWeightRange && 120 < averageVolumeRange}")
     println("Груз с весом 20 кг и объемом 80 л соответствует категории 'Average': ${20 in averageWeightRange && 80 < averageVolumeRange}")
     println("Груз с весом 50 кг и объемом 100 л соответствует категории 'Average': ${50 in averageWeightRange && 100 < averageVolumeRange}")
 }
