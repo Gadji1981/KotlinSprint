84f9714363d60f3637f06f09c17213673668e68c
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-09 08:43:08 +0300

    KS-4-1 Solution for task 1 of lesson 4 updated

--- src/main/kotlin/lesson_4/lesson_4_task_1.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_1.kt
@@ -1,7 +1,8 @@
 package lesson_4
+
 const val TABLES_TOTAL = 13
-fun main(){
-    val tablesReservedToday =13
+fun main() {
+    val tablesReservedToday = 13
     val tablesReservedTomorrow = 4
 
     println("Доступность столиков на сегодня: ${tablesReservedToday < TABLES_TOTAL}")
