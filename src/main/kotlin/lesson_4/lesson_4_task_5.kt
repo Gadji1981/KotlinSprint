79ddedd0c6dd249f457e2fcccb7f9ad305cb3744
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-09 17:30:01 +0300

    KS-4-5 Solution for task 4 of lesson 5 updated

--- src/main/kotlin/lesson_4/lesson_4_task_5.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_5.kt
@@ -1,21 +1,23 @@
 package lesson_4
 
+const val IS_SHIP_DAMAGED = true
+val CREW_NUMBER = 55..70
+const val PROVISION_BOXES = 50
+const val IS_WEATHER_FAVORABLE = true
+
 fun main() {
     println("Корабль имеет повреждения? (да/нет)")
-    val shipDamage = readln() == "да"
+    val isShipDamage = readln() == "да"
     println("Число экипажа:")
     val crewNum = readln().toInt()
     println("Число ящиков с провизией:")
     val provisionBoxes = readln().toInt()
     println("Погода благоприятна? (да/нет)")
-    val weather = readln() == "да"
+    val isWeatherFavorable = readln() == "да"
 
-    val sailingCondition = (shipDamage == false && crewNum in 55..70 && provisionBoxes > 50 && weather == true)
-            || (crewNum == 70 && provisionBoxes > 50 && weather == true)
-
-    val isShipDamaged = shipDamage == true
-    val weatherConditions = weather == true
-
+    val sailingCondition = (isShipDamage == IS_SHIP_DAMAGED && crewNum in CREW_NUMBER
+            && provisionBoxes > PROVISION_BOXES && isWeatherFavorable == IS_WEATHER_FAVORABLE)
+            || (crewNum == 70 && provisionBoxes > PROVISION_BOXES && isWeatherFavorable == IS_WEATHER_FAVORABLE)
 
     println("Может ли корабль отправиться в плавание? $sailingCondition")
 }
