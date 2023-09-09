0b899276fdeca3c4a57d24c9c84154cd60138009
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-09 15:44:57 +0300

    KS-4-4 Solution for task 4 of lesson 4 updated

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -2,16 +2,19 @@ package lesson_4
 
 fun main() {
     val day = 5
-    val armsTraining = day in 0..10 step 2
-    val legsTraining = day in 0..10 step 2
-    val backTraining = day in 1..10 step 2
-    val absTraining = day in 1..10 step 2
+    val isDayEven = day % 2 == 0
+    val armsTraining = isDayEven == true
+    val legsTraining = isDayEven == true
+    val backTraining = isDayEven == false
+    val absTraining = isDayEven == false
 
-    println("""
+    println(
+        """
         |Упражнение для рук: $armsTraining
         |Упражнение для ног: $legsTraining
         |Упражнение для спины: $backTraining
         |Упражнение для пресса: $absTraining
         
-    """.trimMargin())
+    """.trimMargin()
+    )
 }
