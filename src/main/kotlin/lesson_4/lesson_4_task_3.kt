7d009ebd302de22eb585b4fa595cb8385518a725
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-09 15:24:05 +0300

    KS-4-3 Solution for task 3 of lesson 4 updated

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -1,12 +1,17 @@
 package lesson_4
 
+const val IS_SUNNY = true
+const val IS_AWNINIG_OPEN = true
+const val AIR_HUMID = 20
+const val CURENT_SEASON = "не зима"
 fun main() {
-    val weatherToday = true
-    val tentOpen = true
+    val isSunny = true
+    val isAwningOpen = true
     val airHumid = 20
     val curentSeason = "зима"
 
-    val favorableConditions = weatherToday == true && tentOpen == true && airHumid == 20 && curentSeason == "не зима"
+    val favConditions = isSunny == IS_SUNNY && isAwningOpen == IS_AWNINIG_OPEN && airHumid == AIR_HUMID
+            && curentSeason == CURENT_SEASON
 
-    println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
+    println("Благоприятные ли условия сейчас для роста бобовых? $favConditions")
 }
