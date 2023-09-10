f1afc03b04d99a896cd2d85992ebc131df23a6d4
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-10 17:01:06 +0300

    KS-5-1 Solution for task 1 of lesson 5 updated

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -1,10 +1,11 @@
 package lesson_5
 
-const val BOT_CHECK_ANSWER = 4
 fun main() {
     print("Введите ответ для подтверждения что Вы не бот: 2+2=")
     val botCheck = readln().toInt()
 
-    if (botCheck == BOT_CHECK_ANSWER) println("Добро пожаловать!")
+    val botCheckAnswer = 4
+
+    if (botCheck == botCheckAnswer) println("Добро пожаловать!")
     else println("Доступ запрещен.")
 }
