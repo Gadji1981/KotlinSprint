216c7af09243931584a0388041742ad0145149d5
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-11 17:26:29 +0300

    KS-4-2 Solution for task 2 of lesson 4 updated... got rid of "the magic numbers"))

--- src/main/kotlin/lesson_4/lesson_4_task_2.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_2.kt
@@ -9,7 +9,7 @@ fun main() {
 
     println(
         "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average':" +
-                "${20 in AVERAGE_WEIGHT_MIN..AVERAGE_WEIGHT_MAX && 80 < AVERAGE_VOLUME_MAX}"
+                "${weight in AVERAGE_WEIGHT_MIN..AVERAGE_WEIGHT_MAX && volume < AVERAGE_VOLUME_MAX}"
     )
 
     weight = 50
@@ -17,6 +17,6 @@ fun main() {
 
     println(
         "Груз с весом $weight кг и объемом $volume соответствует категории 'Average':" +
-                "${50 in AVERAGE_WEIGHT_MIN..AVERAGE_WEIGHT_MAX && 100 < AVERAGE_VOLUME_MAX}"
+                "${weight in AVERAGE_WEIGHT_MIN..AVERAGE_WEIGHT_MAX && volume < AVERAGE_VOLUME_MAX}"
     )
 }
