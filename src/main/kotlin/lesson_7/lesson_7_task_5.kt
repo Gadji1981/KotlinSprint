0045fa1b2f6be94c88a71dc27a78985702b96918
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-17 13:04:12 +0300

    KS-7-5 Solution for task 5 of lesson 7 updated

--- src/main/kotlin/lesson_7/lesson_7_task_5.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_5.kt
@@ -6,11 +6,9 @@ fun main() {
     val numRange = 0..9
     val lowerCharRange = 'a'..'z'
     val upperCharRange = 'A'..'Z'
-    var pass = ""
 
     for (i in 1..stringLength) {
         val randomSymbol = (numRange + lowerCharRange + upperCharRange).random()
         print(randomSymbol)
-        pass += randomSymbol
     }
 }
