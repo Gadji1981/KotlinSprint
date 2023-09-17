246fcd5f1882d0dd3a6dfcafbf37e748239934ea
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-17 12:27:14 +0300

    KS-8-2 Solution for task 2 of lesson 8

---
+++ src/main/kotlin/lesson_8/lesson_8_task_2.kt
@@ -0,0 +1,21 @@
+package lesson_8
+
+fun main() {
+    val ingredients = arrayOf("Яйцо", "Молоко", "Перец", "Масло", "Помидор", "Зелень")
+
+    println("Введите ингредиент, который вы хотите найти:")
+    val userInput = readln()
+
+    var isFound = false
+
+    for (ingredient in ingredients) {
+        if (ingredient.equals(userInput, ignoreCase = true) == true) {
+            isFound = true
+        }
+    }
+    if (isFound) {
+        println("Ингредиент \"$userInput\" в рецепте есть.")
+    } else {
+        println("Такого ингредиента в рецепте нет")
+    }
+}
