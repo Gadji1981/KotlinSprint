f24dde1081477d4295f916f92e74186016ff9d38
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-17 10:35:01 +0300

    KS-7-3 Solution for task 3 of lesson 7 update

--- src/main/kotlin/lesson_7/lesson_7_task_3.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_3.kt
@@ -5,7 +5,7 @@ fun main() {
     val inputNum = readln().toInt()
     val lastNum = inputNum
 
-    val evenNumber = (0..lastNum step 2).joinToString(", ")
+    val evenNum = (0..lastNum step 2).joinToString(", ")
 
-    println("Все четные числа от 0 до $lastNum \n$evenNumber")
+    println("Все четные числа от 0 до $lastNum \n$evenNum")
 }
