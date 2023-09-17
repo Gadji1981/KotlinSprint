ad4d69349f126d8f71a75d36177578c2dec761fc
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-17 13:57:58 +0300

    KS-8-1 Solution for task 1 of lesson 8

--- src/main/kotlin/lesson_8/lesson_8_task_1.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_1.kt
@@ -1,7 +1,7 @@
 package lesson_8
 
 fun main() {
-    val viewsPerDay = arrayOf(120, 170, 90, 65, 121, 210, 137)
+    val viewsPerDay = arrayOf(113, 170, 90, 65, 121, 210, 137)
     var totalViews = 0
 
     for (views in viewsPerDay) {
