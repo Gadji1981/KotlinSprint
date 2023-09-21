6eee60d9bcc2b3de72b588f6ce65e32bf967e97a
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-21 17:34:16 +0300

    KS-9-2 Solution for task 2 of lesson 9 updated

--- src/main/kotlin/lesson_9/lesson_9_task_2.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_2.kt
@@ -2,18 +2,26 @@ package lesson_9
 
 fun main() {
     val ingredients = mutableListOf("Мясо", "Мука", "Молоко")
-    println("В рецепте есть базовые ингредиенты: $ingredients")
+    println("В рецепте есть базовые ингредиенты:")
+    for (ingredient in ingredients) {
+        println(ingredient)
+    }
 
     while (true) {
         println("Желаете добавить еще? да/нет")
         val answerInput = readln()
 
-        if (answerInput.lowercase() == "да") {
+        if (answerInput.equals("да", ignoreCase = true)) {
             println("Какой ингредиент вы хотите добавить?")
             val newIngredient = readln()
             ingredients.add(newIngredient)
-            println("Теперь в рецепте есть следующие ингредиенты: $ingredients")
-        }else{
+
+            println("Теперь в рецепте есть следующие ингредиенты:")
+            for (ingredient in ingredients) {
+                println(ingredient)
+            }
+            return
+        } else {
             break
         }
     }
