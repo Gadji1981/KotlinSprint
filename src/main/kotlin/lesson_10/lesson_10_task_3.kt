73abb86f3c9c36e7554252ff6c6d1044d8cd18af
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-24 20:42:35 +0300

    KS-10-3 Solution for task 3 of lesson 10

--- src/main/kotlin/lesson_10/lesson_10_task_3.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_3.kt
@@ -1,28 +1,29 @@
 package lesson_10
 
-import kotlin.random.Random
 fun main() {
     println("Enter length of generating pass")
     val passLengthInput = readln().toInt()
 
-    val generatedPass = passGenerator(passLengthInput)
+    val generatedPass = generatePassword(passLengthInput)
 
     println(generatedPass)
 }
 
-fun passGenerator(passLengthInput: Int): String {
+fun generatePassword(passLengthInput: Int): String {
 
-    val digits = "0123456789"
-    val chars = "!\"#\$%&'()*+,-./"
+    val digits = 0..9
+//    val chars = "!\"#\$%&'()*+,-./"
+    val asciiChars = 32..47
+    val chars = asciiChars.map { it.toChar() }
 
     val pass = StringBuilder()
 
     for (i in 0 until passLengthInput) {
         if (i % 2 == 0) {
-            val randomDigits = digits[Random.nextInt(digits.length)]
+            val randomDigits = digits.random()
             pass.append(randomDigits)
         } else {
-            val randomChars = chars[Random.nextInt(chars.length)]
+            val randomChars = chars.random()
             pass.append(randomChars)
         }
     }
