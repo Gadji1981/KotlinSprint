3cefaf25a33cb3a4456db9552f27d1e90dc3bc50
Абдурахман Чилилов <gadji1981@gmail.com>
2023-09-25 20:06:13 +0300

    KS-10-2 Solution for task 2 of lesson 10 update

--- src/main/kotlin/lesson_10/lesson_10_task_2.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_2.kt
@@ -7,11 +7,11 @@ fun main() {
     println("Введите пароль:")
     val password = readln()
 
-    if (loginAndPassCheck(login, password) == true) {
+    if (checkLoginAndPass(login, password)) {
         println("Приветствуем вас, $login!")
     } else {
         println("Логин или пароль недостаточно длинные")
     }
 }
 
-fun loginAndPassCheck(login: String, password: String) = login.length > 4 || password.length > 4
+fun checkLoginAndPass(login: String, password: String) = login.length > 4 || password.length > 4
