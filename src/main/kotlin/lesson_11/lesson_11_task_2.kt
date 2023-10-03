c1ca3b3052bc3af8980ce52f0b26cbfb5a19555a
Абдурахман Чилилов <gadji1981@gmail.com>
2023-10-03 23:51:28 +0300

    KS-11-2 Solution for task 2 of lesson 11 updated

--- src/main/kotlin/lesson_11/lesson_11_task_2.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_2.kt
@@ -1,5 +1,46 @@
 package lesson_11
 
+class User(val id: Int, val userName: String, var password: String, val userEmail: String, var bio: String? = null) {
+
+    fun displayUserInfo() {
+        println(
+            """
+            | Информация о пользователе
+            | Id: $id
+            | Имя: $userName
+            | Пароль: $password
+            | Почта: $userEmail
+            | О себе: $bio
+        """.trimMargin()
+        )
+    }
+
+    fun fillBioData() {
+        println("Введите информацию о себе:")
+        bio = readln()
+    }
+
+    fun changeUserPassword() {
+        println("Введите существующий пароль:")
+        val userPasswordInput = readln()
+
+        if (userPasswordInput == password) {
+            println("Введите новый пароль:")
+            val newPassword = readln()
+            password = newPassword
+            println("Пароль изменен.")
+        } else {
+            println("Пароль введен не верно.")
+        }
+    }
+
+    fun sendEmail(message: String) {
+        println("Почта получателя: $userEmail: \n$message")
+    }
+
+
+}
+
 fun main() {
     val userOne = User(1, "Vasya", "qwerty", "vasya@mail.ru")
     //val userTwo = User(2, "Bob", "12345", "bob@gmail.com")
