be5160d9b61a9ad6956b69d3b392fe2229061602
Абдурахман Чилилов <gadji1981@gmail.com>
2023-10-03 23:48:16 +0300

    KS-11-1 Solution for task 1 of lesson 11 updated

--- src/main/kotlin/lesson_11/User.kt
+++
@@ -1,3 +0,0 @@
-package lesson_11
-
-class User(val id: Int, val userName: String, val password: String, val userEmail: String)
--- src/main/kotlin/lesson_11/lesson_11_task_1.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_1.kt
@@ -1,5 +1,12 @@
 package lesson_11
 
+class User(
+    val id: Int,
+    val userName: String,
+    val password: String,
+    val userEmail: String
+)
+
 fun main() {
     val userOne = User(1, "Vasya", "qwerty", "vasya@mail.ru")
     val userTwo = User(2, "Bob", "12345", "bob@gmail.com")
