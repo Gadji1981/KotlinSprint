a886d11f2ae997565298d32543c54dc3b23aa0ec
Абдурахман Чилилов <gadji1981@gmail.com>
2023-10-18 19:58:17 +0300

    KS-13-2 Solution for task 2 of lesson 13 updated

--- src/main/kotlin/lesson_13/lesson_13_task_2.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_2.kt
@@ -3,11 +3,11 @@ package lesson_13
 class PhoneBook1(
     val name: String,
     val phoneNum: Long,
-    val company: String? = null ?: "<не указано>",
+    val company: String? = null,
 )
 
 fun main() {
-    val phoneBook = PhoneBook1("Ростислав", 89123456789)
+    val phoneBook = PhoneBook1("Ростислав", 89123456789, )
 
-    println("-Имя: ${phoneBook.name} \n-Номер: ${phoneBook.phoneNum} \n-Компания: ${phoneBook.company}")
+    println("-Имя: ${phoneBook.name} \n-Номер: ${phoneBook.phoneNum} \n-Компания: ${phoneBook.company ?: "<не указано>"}")
 }
