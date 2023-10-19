1625135fc68e191ebc3428736fbf8eafcd797a4b
Абдурахман Чилилов <gadji1981@gmail.com>
2023-10-19 18:33:34 +0300

    KS-13-5 Solution for task 5 of lesson 13 updated

--- src/main/kotlin/lesson_13/lesson_13_task_5.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_5.kt
@@ -1,10 +1,43 @@
 package lesson_13
 
-class PhoneBook4 ()
+class PhoneBook3(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String?
+)
 
 fun main() {
-    println("Enter phone number:")
-    val phoneNumber = readln().toLong()
+    val phoneBook = mutableListOf<PhoneBook3>()
 
-    println(phoneNumber)
+    while (true) {
+        println("Введите имя (или 'стоп' для завершения):")
+        val name = readln()
+
+        if (name.equals("стоп", ignoreCase = true)) break
+
+        println("Введите номер телефона:")
+        val phoneNumber = readln().toLong()
+
+        if (phoneNumber == null) {
+            println("Неверный формат номера. Запись не будет добавлена.")
+            continue
+        }
+
+        println("Введите компанию (или нажмите Enter для пропуска):")
+        val company = readLine()
+
+        if (company == "") phoneBook.add(PhoneBook3(name = name, phoneNumber = phoneNumber, company = null))
+        else phoneBook.add(PhoneBook3(name = name, phoneNumber = phoneNumber, company = company))
+
+
+    }
+
+    for (contacts in phoneBook) {
+        println("""
+            |Имя: ${contacts.name}
+            |Номер: ${contacts.phoneNumber}
+            |Компания: ${contacts.company ?: "<не указано>"}
+            |
+        """.trimMargin())
+    }
 }
