14ea10f1ff719f17018ca30bf1959da19d17c811
Абдурахман Чилилов <gadji1981@gmail.com>
2023-10-22 10:56:47 +0300

    KS-13-5 Solution for task 5 of lesson 13 updated

--- src/main/kotlin/lesson_13/lesson_13_task_5.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_5.kt
@@ -7,37 +7,10 @@ class PhoneBook4(
 )
 
 fun main() {
-    val phoneBook = mutableListOf<PhoneBook4>()
-
-    while (true) {
-        println("Введите имя (или 'стоп' для завершения):")
-        val name = readln()
-
-        if (name.equals("стоп", ignoreCase = true)) break
-
+    try {
         println("Введите номер телефона:")
         val phoneNumber = readln().toLong()
-
-        if (phoneNumber == null) {
-            println("Неверный формат номера. Запись не будет добавлена.")
-            continue
-        }
-
-        println("Введите компанию (или нажмите Enter для пропуска):")
-        val company = readLine()
-
-        if (company == "") phoneBook.add(PhoneBook4(name = name, phoneNumber = phoneNumber, company = null))
-        else phoneBook.add(PhoneBook4(name = name, phoneNumber = phoneNumber, company = company))
-
-
-    }
-
-    for (contacts in phoneBook) {
-        println("""
-            |Имя: ${contacts.name}
-            |Номер: ${contacts.phoneNumber}
-            |Компания: ${contacts.company ?: "<не указано>"}
-            |
-        """.trimMargin())
+    } catch (e: NumberFormatException) {
+        println("Ошибка ввода номера телефона")
     }
 }
